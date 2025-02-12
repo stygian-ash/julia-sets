ee9b7bb865f06f09416df4dd8d537b2659a88a1d
Emily Prieto <61399347+stygian-ash@users.noreply.github.com>
2025-02-12 13:26:01 -0500

    Add labels and window

--- visualize.py
+++ visualize.py
@@ -36,22 +36,33 @@ def quiver_ex():
 
 if __name__ == '__main__':
 	fig, axs = plt.subplots(2, 2)
+	xmin, xmax = -10, 10
+	ymin, ymax = xmin, xmax
 	f = lambda z: z**2
 	mp.cplot(lambda z: z, points=2e4, axes=axs[0][0],
-		  re=[-10, 10], im=[-10, 10])
+		  re=[xmin, xmax], im=[ymin, ymax], color=None)
+	axs[0][0].set_title('Domain')
 	mp.cplot(f, points=2e4, axes=axs[0][1],
-		  re=[-10, 10], im=[-10, 10])
+		  re=[xmin, xmax], im=[ymin, ymax], color=None)
+	axs[0][1].set_title('Range')
 
-	x, y = np.ogrid[-10:10:1, -10:10:1]
+	# axs3d = fig.add_subplot(1, 2, 1, projection='3d')
+	# mp.splot(lambda x, y: np.abs(f(x + 1j*y)),
+	# 	  u=[xmin, xmax], v=[ymin, ymax],
+	# 	  axes=axs3d)
+
+
+	x, y = np.ogrid[xmin:xmax:20j, ymin:ymax:20j]
 	zs = np.ndarray.flatten(x + y * 1j)
 	ds = f(zs) - zs
 	xs = [z.real for z in zs]
 	ys = [z.imag for z in zs]
 	us = [z.real for z in ds]
 	vs = [z.imag for z in ds]
-	axs[1][0].set_xlabel('Re(z)')
-	axs[1][0].set_ylabel('Im(z)')
-	axs[1][0].quiver(xs, ys, us, vs,
+	axs[1][1].set_xlabel('Re(z)')
+	axs[1][1].set_ylabel('Im(z)')
+	axs[1][1].set_title('Flow Field')
+	axs[1][1].quiver(xs, ys, us, vs,
 				  angles='xy', width=2.5e-3)
 
 	plt.show()

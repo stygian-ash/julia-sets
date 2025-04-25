5b15ad1833feba33c2ec14cd29c21d29beaac34f
Emily Prieto <61399347+stygian-ash@users.noreply.github.com>
2025-04-25 01:12:03 -0400

    Add logos and move header images

--- figures/ticks.py
+++ figures/ticks.py
@@ -6,19 +6,19 @@ import sys
 if __name__ == '__main__':
 	img = mpimg.imread(sys.argv[1])
 
-	plt.rcParams.update({"ytick.color" : "w",
-						 "xtick.color" : "w",
-						 "axes.labelcolor" : "w",
-						 "axes.edgecolor" : "w"})
+	# plt.rcParams.update({"ytick.color" : "w",
+	# 					 "xtick.color" : "w",
+	# 					 "axes.labelcolor" : "w",
+	# 					 "axes.edgecolor" : "w"})
 
 	imgplot = plt.imshow(img,
 					  extent=(-1.5, .7, -1.1, 1.1))
 
 	if len(sys.argv) >= 3:
-		plt.title(sys.argv[2], size=24, color='white')
-	plt.title(r'$\{c \in \mathbf{C} : 0 \in K(Q_c)\}$', size=24, color='white')
-	plt.xlabel(r'$\operatorname{Re}(c)$', color='white')
-	plt.ylabel(r'$\operatorname{Im}(c)$', color='white')
+		plt.title(sys.argv[2], size=24)
+	# plt.title(r'$\{c \in \mathbf{C} : 0 \in K(Q_c)\}$', size=24)
+	plt.xlabel(r'$\operatorname{Re}(c)$', color='k')
+	plt.ylabel(r'$\operatorname{Im}(c)$', color='k')
 
 	if len(sys.argv) >= 4:
 		plt.savefig(sys.argv[3], transparent=True)
